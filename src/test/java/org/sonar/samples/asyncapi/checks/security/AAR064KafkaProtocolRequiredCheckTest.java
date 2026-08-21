e2c53049449cc556c5204336722f55284f2d6143
Melsy Huamani <melsyhuamanivargas@gmail.com>
2026-08-21 08:01:21 -0500

    feat: add tests for coverage AAR064

--- src/test/java/org/sonar/samples/asyncapi/checks/security/AAR064KafkaProtocolRequiredCheckTest.java
+++ src/test/java/org/sonar/samples/asyncapi/checks/security/AAR064KafkaProtocolRequiredCheckTest.java
@@ -35,6 +35,61 @@ public class AAR064KafkaProtocolRequiredCheckTest extends BaseCheckTest {
     }
 
     @Test
+    public void verifyV2InvalidWrongCase() {
+        verifyV2("invalid-wrong-case.yaml");
+    }
+
+    @Test
+    public void verifyV2InvalidWhitespace() {
+        verifyV2("invalid-whitespace.yaml");
+    }
+
+    @Test
+    public void verifyV2InvalidNumeric() {
+        verifyV2("invalid-numeric.yaml");
+    }
+
+    @Test
+    public void verifyV2InvalidObjectProtocol() {
+        verifyV2("invalid-object-protocol.yaml");
+    }
+
+    @Test
+    public void verifyV2InvalidEmptyObjectArrayProtocol() {
+        verifyV2("invalid-empty-object-array-protocol.yaml");
+    }
+
+    @Test
+    public void verifyV2InvalidMixed() {
+        verifyV2("invalid-mixed.yaml");
+    }
+
+    @Test
+    public void verifyV2SkipCases() {
+        verifyV2("skip-cases.yaml");
+    }
+
+    @Test
+    public void verifyV2NoServers() {
+        verifyV2("no-servers.yaml");
+    }
+
+    @Test
+    public void verifyV2NullServers() {
+        verifyV2("null-servers.yaml");
+    }
+
+    @Test
+    public void verifyV2EmptyServers() {
+        verifyV2("empty-servers.yaml");
+    }
+
+    @Test
+    public void verifyV2RefServer() {
+        verifyV2("ref-server.yaml");
+    }
+
+    @Test
     public void verifyV3Valid() {
         verifyV3("valid.yaml");
     }
@@ -47,6 +102,66 @@ public class AAR064KafkaProtocolRequiredCheckTest extends BaseCheckTest {
     @Test
     public void verifyV3InvalidArray() {
         verifyV3("invalid-array.yaml");
+    }
+
+    @Test
+    public void verifyV3InvalidArrayMixed() {
+        verifyV3("invalid-array-mixed.yaml");
+    }
+
+    @Test
+    public void verifyV3InvalidNonstring() {
+        verifyV3("invalid-nonstring.yaml");
+    }
+
+    @Test
+    public void verifyV3InvalidEmptyAndWhitespace() {
+        verifyV3("invalid-empty-and-whitespace.yaml");
+    }
+
+    @Test
+    public void verifyV3InvalidWrongCase() {
+        verifyV3("invalid-wrong-case.yaml");
+    }
+
+    @Test
+    public void verifyV3InvalidObjectAndArrayProtocol() {
+        verifyV3("invalid-object-and-array-protocol.yaml");
+    }
+
+    @Test
+    public void verifyV3SkipCases() {
+        verifyV3("skip-cases.yaml");
+    }
+
+    @Test
+    public void verifyV3NoServers() {
+        verifyV3("no-servers.yaml");
+    }
+
+    @Test
+    public void verifyV3NullServers() {
+        verifyV3("null-servers.yaml");
+    }
+
+    @Test
+    public void verifyV3EmptyServersObject() {
+        verifyV3("empty-servers-object.yaml");
+    }
+
+    @Test
+    public void verifyV3EmptyServersArray() {
+        verifyV3("empty-servers-array.yaml");
+    }
+
+    @Test
+    public void verifyV3RefServer() {
+        verifyV3("ref-server.yaml");
+    }
+
+    @Test
+    public void verifyV31Valid() {
+        verifyV31("valid.yaml");
     }
 
     @Test
